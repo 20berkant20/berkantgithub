5944faa437f3ddebb521a5c26e0020cbeb596cbe
Berkant Sama <0331540@student.rocvantwente.nl>
2020-11-29 21:08:07 +0100

    Veel moeite gehad hiermee

--- Invoice Application/Invoice Application/Form1.Designer.cs
+++ Invoice Application/Invoice Application/Form1.Designer.cs
@@ -29,13 +29,140 @@ namespace Invoice_Application
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Subtotal = new System.Windows.Forms.Label();
+            this.DiscountPercent = new System.Windows.Forms.Label();
+            this.DiscountAmount = new System.Windows.Forms.Label();
+            this.Total = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(207, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(91, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 67);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(91, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(141, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(91, 22);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(141, 141);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(91, 22);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Subtotal
+            // 
+            this.Subtotal.AutoSize = true;
+            this.Subtotal.Location = new System.Drawing.Point(23, 33);
+            this.Subtotal.Name = "Subtotal";
+            this.Subtotal.Size = new System.Drawing.Size(60, 17);
+            this.Subtotal.TabIndex = 10;
+            this.Subtotal.Text = "Subtotal";
+            // 
+            // DiscountPercent
+            // 
+            this.DiscountPercent.AutoSize = true;
+            this.DiscountPercent.Location = new System.Drawing.Point(23, 70);
+            this.DiscountPercent.Name = "DiscountPercent";
+            this.DiscountPercent.Size = new System.Drawing.Size(112, 17);
+            this.DiscountPercent.TabIndex = 11;
+            this.DiscountPercent.Text = "DiscountPercent";
+            // 
+            // DiscountAmount
+            // 
+            this.DiscountAmount.AutoSize = true;
+            this.DiscountAmount.Location = new System.Drawing.Point(23, 102);
+            this.DiscountAmount.Name = "DiscountAmount";
+            this.DiscountAmount.Size = new System.Drawing.Size(111, 17);
+            this.DiscountAmount.TabIndex = 12;
+            this.DiscountAmount.Text = "DiscountAmount";
+            // 
+            // Total
+            // 
+            this.Total.AutoSize = true;
+            this.Total.Location = new System.Drawing.Point(23, 139);
+            this.Total.Name = "Total";
+            this.Total.Size = new System.Drawing.Size(40, 17);
+            this.Total.TabIndex = 13;
+            this.Total.Text = "Total";
+            this.Total.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 235);
+            this.Controls.Add(this.Total);
+            this.Controls.Add(this.DiscountAmount);
+            this.Controls.Add(this.DiscountPercent);
+            this.Controls.Add(this.Subtotal);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label Subtotal;
+        private System.Windows.Forms.Label DiscountPercent;
+        private System.Windows.Forms.Label DiscountAmount;
+        private System.Windows.Forms.Label Total;
+    }
 }
 
--- Invoice Application/Invoice Application/Form1.cs
+++ Invoice Application/Invoice Application/Form1.cs
@@ -10,11 +10,75 @@ using System.Windows.Forms;
 
 namespace Invoice_Application
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        private object txtlabel3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       private decimal Button2_Click(object sender, EventArgs e)
+        {
+            decimal label3; * label1
+            decimal label4; - label3
+
+           
+            label3.ToString;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+      
+    }  
+    }
 }
